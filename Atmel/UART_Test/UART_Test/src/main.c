bf31c758a25a5e25de8a699b8851d557056a3ca5
hugoecarl <mormeme@gmail.com>
2019-03-18 18:51:56 -0300

    Oi

--- Atmel/UART_Test/UART_Test/src/main.c
+++ Atmel/UART_Test/UART_Test/src/main.c
@@ -31,6 +31,12 @@
 #include <asf.h>
 #include <string.h>
 
+// defines do Botao de start
+#define BUTTSTART_PIO           PIOC
+#define BUTTSTART_PIO_ID        ID_PIOC
+#define BUTTSTART_PIO_IDX       17u
+#define BUTTSTART_PIO_IDX_MASK  (1u << BUTTSTART_PIO_IDX)
+
 // Descomente o define abaixo, para desabilitar o Bluetooth e utilizar modo Serial via Cabo
 //#define DEBUG_SERIAL
 
@@ -110,10 +116,10 @@ int hc05_server_init(void) {
 	char buffer_rx[128];
 	usart_send_command(USART0, buffer_rx, 1000, "AT", 1000);
 	usart_send_command(USART0, buffer_rx, 1000, "AT", 1000);	
-	usart_send_command(USART0, buffer_rx, 1000, "AT+NAMEServer", 1000);
+	usart_send_command(USART0, buffer_rx, 1000, "AT+NAMEHUGO", 1000);
 	usart_log("hc05_server_init", buffer_rx);
 	usart_send_command(USART0, buffer_rx, 1000, "AT", 1000);
-	usart_send_command(USART0, buffer_rx, 1000, "AT+PIN0000", 1000);
+	usart_send_command(USART0, buffer_rx, 1000, "AT+PIN4567", 1000);
 	usart_log("hc05_server_init", buffer_rx);
 }
 
@@ -137,8 +143,14 @@ int main (void)
 	char eof = 'X';
 	char buffer[1024];
 	
+	pmc_enable_periph_clk(BUTTSTART_PIO_ID);
+	// configura pino ligado ao bot?o como entrada com um pull-up.
+	pio_set_input(BUTTSTART_PIO,BUTTSTART_PIO_IDX_MASK,PIO_DEFAULT);
+	// ativa pullup
+	pio_pull_up(BUTTSTART_PIO, BUTTSTART_PIO_IDX_MASK, 1);
+	
 	while(1) {
-		if(pio_get(PIOA, PIO_INPUT, PIO_PA11) == 0) {
+		if(pio_get(BUTTSTART_PIO,PIO_INPUT,BUTTSTART_PIO_IDX_MASK) == 0) {
 			button1 = '1';
 		} else {
 			button1 = '0';
@@ -148,5 +160,7 @@ int main (void)
 		usart_write(UART_COMM, button1);
 		while(!usart_is_tx_ready(UART_COMM));
 		usart_write(UART_COMM, eof);
+				delay_ms(250);
+
 	}
 }
